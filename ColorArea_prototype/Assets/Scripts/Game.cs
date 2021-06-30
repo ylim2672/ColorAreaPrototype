85deafa201bdcdfc9438bf8364c300ec4bbc9546
ylim2672 <74486880+ylim2672@users.noreply.github.com>
2021-07-01 00:04:02 +0900

    start
    start the unity

---
+++ ColorArea_prototype/Assets/Scripts/Game.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject token;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(token, new Vector3(0, 0, -1), Quaternion.identity);
+    }
+}
